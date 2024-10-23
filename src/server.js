c86be6cd7ed924b312960bfce041dd833d47a414
Darya-Karnaukh <153089841+Darya-Karnaukh@users.noreply.github.com>
2024-10-24 00:27:46 +0300

    homework07

--- src/server.js
+++ src/server.js
@@ -7,8 +7,10 @@ import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import router from './routers/index.js';
 import cookieParser from 'cookie-parser';
 import { UPLOAD_DIR } from './constants/index.js';
+import { swaggerDocs } from './middlewares/swaggerDocs.js';
 
 const PORT = Number(env('PORT'));
+
 export const setupServer = () => {
   const app = express();
 
@@ -30,6 +32,7 @@ export const setupServer = () => {
     });
   });
   app.use('/uploads', express.static(UPLOAD_DIR));
+  app.use('/api-docs', swaggerDocs());
 
   app.use(router);
 

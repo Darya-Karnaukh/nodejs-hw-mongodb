4052865e47a2ad093c8c0b7d323e4e834ddc64d0
Darya-Karnaukh <153089841+Darya-Karnaukh@users.noreply.github.com>
2025-02-28 10:57:06 +0200

    update cors

--- src/server.js
+++ src/server.js
@@ -15,7 +15,17 @@ export const setupServer = () => {
   const app = express();
 
   app.use(express.json());
-  app.use(cors());
+  const allowedOrigins = [
+    'http://localhost:3000',
+    'https://your-react-app-url.com',
+  ];
+  app.use(
+    cors({
+      origin: allowedOrigins,
+      methods: ['GET', 'POST', 'PUT', 'DELETE'],
+      credentials: true,
+    }),
+  );
   app.use(cookieParser());
 
   app.use(
